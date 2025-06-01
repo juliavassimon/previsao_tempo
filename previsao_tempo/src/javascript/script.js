f590c0ae7af91ebbaed15b20e2c591b94764fd94
Júlia Vassimon <juliavassimonsilva@gmail.com>
2025-06-01 18:26:21 -0300

    Update script.js

--- previsao_tempo/src/javascript/script.js
+++ previsao_tempo/src/javascript/script.js
@@ -4,11 +4,11 @@ document.querySelector('#search').addEventListener('submit', async (event) => {
 
     if (!cityName) {
         document.querySelector("#weather").classList.remove('show');
-        showAlert('Você precisa digitar uma cidade!');
+        showAlert('VocÃª precisa digitar uma cidade!');
         return
     }
 
-    const senhaApi = '114f3e120a107b1d82480b4ca3fdeef5';
+    const senhaApi = 'senha fornecida;
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${senhaApi}&units=metric&lang=pt_br`;
 
     const results = await fetch(apiUrl);
@@ -28,7 +28,7 @@ document.querySelector('#search').addEventListener('submit', async (event) => {
         });
     } else {
         document.querySelector("#weather").classList.remove('show');
-        showAlert(`Não foi possível localizar... <img src="src/imagem/404.svg">`);
+        showAlert(`NÃ£o foi possÃ­vel localizar... <img src="src/imagem/404.svg">`);
     }
     
 });
@@ -37,11 +37,11 @@ function showInfos(json) {
     showAlert('');
     document.querySelector('#weather').classList.add("show");
     document.querySelector('#title').innerHTML = `${json.city}, ${json.country}`; 
-    document.querySelector('#tempo_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.',',')} <sup>°C</sup>`; 
+    document.querySelector('#tempo_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.',',')} <sup>Â°C</sup>`; 
     document.querySelector('#tempo_description').innerHTML = `${json.description}`; 
     document.querySelector('#temp_img').setAttribute('src',`https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
-    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.',',')} <sup>°C</sup>`; 
-    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.',',')} <sup>°C</sup>`; 
+    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace('.',',')} <sup>Â°C</sup>`; 
+    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace('.',',')} <sup>Â°C</sup>`; 
     document.querySelector('#wind').innerHTML = `${json.windSpeed}Km/h`; 
     document.querySelector('#humidity').innerHTML = `${json.humidity}%`; 
 
